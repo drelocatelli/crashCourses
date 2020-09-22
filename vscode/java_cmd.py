28596580879cd822759eb15c748dbedfe4ebd0be
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-09-22 17:15:28 -0300

    Update java_cmd.py

--- vscode/java_cmd.py
+++ vscode/java_cmd.py
@@ -1,6 +1,11 @@
 import os
 
-print(os.getcwd()+"\n\n")
+print(os.getcwd()+"\n")
+files = os.listdir()
+for file in files:
+    if file.endswith('.java'):
+        print(file)
+print('\n')
 java_file = input("[nome do arquivo] $ ")
 cmd = f"javac -encoding UTF8 -g:none {java_file} "
 cmd2 = f"java {java_file}"
